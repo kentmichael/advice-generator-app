e270c1bf7a1c973b4abc8f12f5dcbfe17a9b1c44
Kent Michael San <kentmichaelsan22@gmail.com>
2022-03-28 01:03:33 +0800

    create main app functionality

---
+++ README.md
@@ -0,0 +1,22 @@
+# Frontend Mentor - Advice generator app solution
+
+- This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## The challenge
+
+- Challenge is to build out this advice generator app using the [Advice Slip API](https://api.adviceslip.com) and get it looking as close to the design as possible.
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+### Links
+
+- Live Site URL: https://kentmichael.github.io/advice-generator-app/
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- Mobile-first workflow
--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,47 @@
-console.log('Running...');
+/*
+  Random Advice Generator
+  By default, on first load the app will request for a
+  random advice and display it to the card. Then, the Class
+  Advice will have the dice button listen to a 'click' event
+  to request for another random advice and replace the first
+  one in the card.
+*/
+class Advice{
+  constructor(diceButton, displayMessage, displayNumber){
+    this.diceButton = diceButton;
+    this.displayMessage = displayMessage;
+    this.displayNumber = displayNumber;
+
+    diceButton.addEventListener('click', this.randomAdvice);
+  }
+
+  randomAdvice = () => {
+    let slip_id = Math.ceil(Math.random() * 224);
+    fetch(`https://api.adviceslip.com/advice/${slip_id}`).
+    then((response) => {
+      if(!response.ok){
+        throw new Error(response.status);
+      }
+      return response.json();
+    }).
+    then((data) => {
+      this.displayAdvice(data);
+    }).
+    catch((err) => {
+      console.error(err);
+    });
+  }
+
+  displayAdvice = ({slip}) => {
+    const {id, advice} = slip;
+    this.displayMessage.innerText = advice;
+    this.displayNumber.innerText = id;
+  }
+}
+
+const diceButton = document.querySelector('.dice-btn');
+const displayMessage = document.querySelector('.advice-msg');
+const displayNumber = document.querySelector('h1 .advice-num');
+const adviceOnLoad = new Advice(diceButton, displayMessage, displayNumber);
+
+adviceOnLoad.randomAdvice();
